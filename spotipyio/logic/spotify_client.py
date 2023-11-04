63c0eff67b6c1e87b165d36beefe6dc76c19bf72
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-11-04 21:14:54 +0200

    Add playlists collector to spotify_client

--- spotipyio/logic/spotify_client.py
+++ spotipyio/logic/spotify_client.py
@@ -1,9 +1,9 @@
 from aiohttp import ClientSession
 
 from spotipyio.logic.collectors.chunks_collectors.albums_collector import AlbumsCollector
-from spotipyio.logic.collectors.chunks_collectors.artists_collector import ArtistsCollector
 from spotipyio.logic.collectors.chunks_collectors.audio_features_collector import AudioFeaturesCollector
 from spotipyio.logic.collectors.chunks_collectors.tracks_collector import TracksCollector
+from spotipyio.logic.collectors.singles_collectors.playlists_collector import PlaylistsCollector
 from spotipyio.logic.managers.artists_manager import ArtistsManager
 
 
@@ -12,11 +12,13 @@ class SpotifyClient:
                  artists_manager: ArtistsManager,
                  albums_collector: AlbumsCollector,
                  tracks_collector: TracksCollector,
-                 audio_features_collector: AudioFeaturesCollector):
+                 audio_features_collector: AudioFeaturesCollector,
+                 playlists_collector: PlaylistsCollector):
         self.artists = artists_manager
         self.albums = albums_collector
         self.tracks = tracks_collector
         self.audio_features = audio_features_collector
+        self.playlists = playlists_collector
 
     @classmethod
     def create(cls, session: ClientSession) -> "SpotifyClient":
@@ -24,5 +26,6 @@ class SpotifyClient:
             artists_manager=ArtistsManager.create(session),
             albums_collector=AlbumsCollector(session),
             tracks_collector=TracksCollector(session),
-            audio_features_collector=AudioFeaturesCollector(session)
+            audio_features_collector=AudioFeaturesCollector(session),
+            playlists_collector=PlaylistsCollector(session)
         )
