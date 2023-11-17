2cc183cffc1781573a263adb5b1a2526f7ebaaf4
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-11-17 11:57:51 +0200

    Export temporarily AccessTokenGenerator as part of package

--- spotipyio/__init__.py
+++ spotipyio/__init__.py
@@ -1,5 +1,7 @@
+from spotipyio.logic.authentication.access_token_generator import AccessTokenGenerator
 from spotipyio.logic.spotify_client import SpotifyClient
 
 __all__ = [
-    "SpotifyClient"
+    "SpotifyClient",
+    "AccessTokenGenerator"
 ]
