d2965346cb0588ffd9387bbf660d235ecc50578a
nir.godin <nir.godin@planckresolution.com>
2023-12-11 19:20:01 +0200

    Make passing params to singles collectors optional

--- spotipyio/contract/collectors/base_single_collector.py
+++ spotipyio/contract/collectors/base_single_collector.py
@@ -1,6 +1,6 @@
 from abc import abstractmethod, ABC
 from functools import partial
-from typing import List
+from typing import List, Optional
 
 from spotipyio.consts.spotify_consts import SPOTIFY_API_BASE_URL
 from spotipyio.contract.spotify_component_interface import ISpotifyComponent
@@ -14,10 +14,10 @@ class BaseSingleCollector(ISpotifyComponent, ABC):
         self._pool_executor = pool_executor
 
     async def run(self, ids: List[str], **params) -> List[dict]:  # TODO: Think how to better externalize mandatory params
-        func = partial(self.run_single, params)
+        func = partial(self.run_single, params=params)
         return await self._pool_executor.run(iterable=ids, func=func, expected_type=dict)
 
-    async def run_single(self, params: dict, id_: str) -> dict:
+    async def run_single(self, id_: str, params: Optional[dict] = None) -> dict:
         route = self._route_format.format(id=id_)
         url = f"{SPOTIFY_API_BASE_URL}/{route}"
 
