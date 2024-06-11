85af661ce0d2b97afc73f146abebbf0028f9e799
nir.godin <nir.godin@planckresolution.com>
2024-06-11 14:14:02 +0300

    Add pagination options

--- spotipyio/logic/collectors/singles_collectors/playlists_collector.py
+++ spotipyio/logic/collectors/singles_collectors/playlists_collector.py
@@ -1,8 +1,35 @@
-from spotipyio.consts.spotify_consts import PLAYLISTS
-from spotipyio.contract import BaseSingleCollector
+from functools import partial
+from typing import List
+
+from spotipyio.consts.spotify_consts import SPOTIFY_API_BASE_URL, PLAYLISTS, TRACKS, NEXT
+from spotipyio.contract import ISpotifyComponent
+from spotipyio.logic.authentication.spotify_session import SpotifySession
+from spotipyio.tools import PoolExecutor
+from spotipyio.utils import safe_nested_get
 
 
-class PlaylistsCollector(BaseSingleCollector):
-    @property
-    def _route_format(self) -> str:
-        return f"{PLAYLISTS}/{{id}}"
+class PlaylistsCollector(ISpotifyComponent):
+    def __init__(self, session: SpotifySession, pool_executor: PoolExecutor = PoolExecutor()):
+        super().__init__(session)
+        self._pool_executor = pool_executor
+
+    async def run(self, ids: List[str], paginate: bool = False) -> List[dict]:
+        func = partial(self.run_single, paginate)
+        return await self._pool_executor.run(iterable=ids, func=func, expected_type=dict)
+
+    async def run_single(self, id_: str, paginate: bool = False) -> dict:
+        url = f"{SPOTIFY_API_BASE_URL}/{PLAYLISTS}/{id_}"
+        playlist = await self._session.get(url=url)
+
+        if paginate:
+            await self._append_additional_pages_items(playlist)
+
+        return playlist
+
+    async def _append_additional_pages_items(self, playlist: dict) -> None:
+        next_url = safe_nested_get(playlist, [TRACKS, NEXT])
+
+        while next_url is not None:
+            page = await self._session.get(url=next_url, params={'additional_types': 'track'})
+            playlist["tracks"]["items"].extend(page)
+            next_url = page["next"]
