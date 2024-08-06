c6249dd49129bf2b78855c6f91cf10e285d89d25
nir.godin <nir.godin@planckresolution.com>
2024-08-06 18:48:02 +0300

    Initialized artists top tracks test component and fixed some imports

--- spotipyio/testing/managers/artists_test_manager.py
+++ spotipyio/testing/managers/artists_test_manager.py
@@ -1,16 +1,18 @@
 from typing import Dict, Type
 
-from spotipyio.testing.components.artists.artists_info_test_component import ArtistsInfoTestComponent
+from spotipyio.testing.components import ArtistsInfoTestComponent, ArtistsTopTracksTestComponent
 from spotipyio.testing.infra import BaseTestManager, BaseTestComponent
 
 
 class ArtistsTestManager(BaseTestManager):
-    def __init__(self, info: ArtistsInfoTestComponent):
+    def __init__(self, info: ArtistsInfoTestComponent, top_tracks: ArtistsTopTracksTestComponent):
         super().__init__()
         self.info = info
+        self.top_tracks = top_tracks
 
     @staticmethod
     def _components() -> Dict[str, Type[BaseTestComponent]]:
         return {
             "info": ArtistsInfoTestComponent,
+            "top_tracks": ArtistsTopTracksTestComponent
         }
