708d78b9f864f534cde7556e1ee196c33b7e02a4
nir.godin <nir.godin@planckresolution.com>
2024-08-11 20:11:03 +0300

    Refactored artists info test component and base chunks test component

--- spotipyio/testing/components/artists/artists_info_test_component.py
+++ spotipyio/testing/components/artists/artists_info_test_component.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from spotipyio.consts.typing_consts import Json
 from spotipyio.testing.infra.base_chunks_test_component import BaseChunksTestComponent
+from spotipyio.testing.spotify_mock_factory import SpotifyMockFactory
 
 
 class ArtistsInfoTestComponent(BaseChunksTestComponent):
@@ -9,3 +13,6 @@ class ArtistsInfoTestComponent(BaseChunksTestComponent):
     @property
     def _chunk_size(self) -> int:
         return 50
+
+    def _random_valid_responses(self, handlers_number: int) -> List[Json]:
+        return [SpotifyMockFactory.several_artists() for _ in range(handlers_number)]
