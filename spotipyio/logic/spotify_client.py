b07e11fe74e4e01707d003651511c0104615e5bc
Nir Godin <70273595+nirgodin@users.noreply.github.com>
2024-09-26 19:11:44 +0300

    Chapters and episodes info collectors (#10)
    * Initialized chapters collector + test component
    
    * Finalized chapters info tests
    
    * Initialized episodes collector + tests
    
    * Fix some imports

--- spotipyio/logic/spotify_client.py
+++ spotipyio/logic/spotify_client.py
@@ -9,7 +9,9 @@ class SpotifyClient:
                  session: SpotifySession,
                  albums_manager: AlbumsManager,
                  artists_manager: ArtistsManager,
+                 chapters_manager: ChaptersManager,
                  current_user_manager: CurrentUserManager,
+                 episodes_manager: EpisodesManager,
                  playlists_manager: PlaylistsManager,
                  search_manager: SearchManager,
                  tracks_manager: TracksManager,
@@ -17,7 +19,9 @@ class SpotifyClient:
         self.session = session
         self.albums = albums_manager
         self.artists = artists_manager
+        self.chapters = chapters_manager
         self.current_user = current_user_manager
+        self.episodes = episodes_manager
         self.playlists = playlists_manager
         self.search = search_manager
         self.tracks = tracks_manager
@@ -28,7 +32,9 @@ class SpotifyClient:
         return SpotifyClient(
             session=session,
             artists_manager=ArtistsManager.create(base_url, session),
+            chapters_manager=ChaptersManager.create(base_url, session),
             current_user_manager=CurrentUserManager.create(base_url, session),
+            episodes_manager=EpisodesManager.create(base_url, session),
             playlists_manager=PlaylistsManager.create(base_url, session),
             users_manager=UsersManager.create(base_url, session),
             albums_manager=AlbumsManager.create(base_url, session),
---
+++ spotipyio/testing/components/chapters/__init__.py
@@ -0,0 +1,5 @@
+from spotipyio.testing.components.chapters.chapters_info_test_component import ChaptersInfoTestComponent
+
+__all__ = [
+    "ChaptersInfoTestComponent"
+]
