57c1fe70fcffeee54d3e6ab5d7dfbfe243cf7d5b
nir.godin <nir.godin@planckresolution.com>
2025-03-01 11:11:41 +0200

    Use method to add default entries to expected

--- tests/models/test_search_item.py
+++ tests/models/test_search_item.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import pytest
 from _pytest.fixtures import fixture
 
@@ -11,23 +13,20 @@ class TestSearchItem:
             SearchItem()
 
     def test_to_query_params__quote_false__returns_non_encoded_params_dict(self, search_item: SearchItem):
-        expected = {
-            "q": "Bridge Over Troubled Water artist:Simon & Garfunkel year:1970",
-            "type": "track,album",
-            OFFSET: "0",
-            LIMIT: "50",
-        }
+        expected = self._expected_with_default_entries(
+            {"q": "Bridge Over Troubled Water artist:Simon & Garfunkel year:1970", "type": "track,album"}
+        )
         actual = search_item.to_query_params()
         assert actual == expected
 
     def test_to_query_params__quote_true__returns_encoded_params_dict(self, search_item: SearchItem):
         search_item.metadata.quote = True
-        expected = {
-            "q": "Bridge%20Over%20Troubled%20Water%20artist%3ASimon%20%26%20Garfunkel%20year%3A1970",
-            "type": "track,album",
-            OFFSET: "0",
-            LIMIT: "50",
-        }
+        expected = self._expected_with_default_entries(
+            {
+                "q": "Bridge%20Over%20Troubled%20Water%20artist%3ASimon%20%26%20Garfunkel%20year%3A1970",
+                "type": "track,album",
+            }
+        )
 
         actual = search_item.to_query_params()
 
@@ -35,7 +34,9 @@ class TestSearchItem:
 
     def test_to_query_params__text_none__includes_only_filters_in_query(self, search_item: SearchItem):
         search_item.text = None
-        expected = {"q": "artist:Simon & Garfunkel year:1970", "type": "track,album", OFFSET: "0", LIMIT: "50"}
+        expected = self._expected_with_default_entries(
+            {"q": "artist:Simon & Garfunkel year:1970", "type": "track,album"}
+        )
 
         actual = search_item.to_query_params()
 
@@ -43,7 +44,7 @@ class TestSearchItem:
 
     def test_to_query_params__no_filters__includes_only_text_in_query(self, search_item: SearchItem):
         search_item.filters = SearchItemFilters()
-        expected = {"q": "Bridge Over Troubled Water", "type": "track,album", OFFSET: "0", LIMIT: "50"}
+        expected = self._expected_with_default_entries({"q": "Bridge Over Troubled Water", "type": "track,album"})
 
         actual = search_item.to_query_params()
 
@@ -56,3 +57,8 @@ class TestSearchItem:
             filters=SearchItemFilters(artist="Simon & Garfunkel", year=1970),
             metadata=SearchItemMetadata(search_types=[SpotifySearchType.TRACK, SpotifySearchType.ALBUM], quote=False),
         )
+
+    @staticmethod
+    def _expected_with_default_entries(expected: Dict[str, str]) -> Dict[str, str]:
+        expected.update({OFFSET: "0", LIMIT: "50"})
+        return expected
